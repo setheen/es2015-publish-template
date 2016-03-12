3e6cc2b23a91164b16907915d5eb6d0bed30c0a0
andywins <andywins@live.com>
2016-03-12 16:23:28 -0600

    more testing

---
+++ lib/index.ts
@@ -0,0 +1,16 @@
+import MathLib from './multiply';
+export {MathLib};
+console.log('here');
+// function add(one, two)
+// {
+//     let result = one + two;
+//     console.log(`Adding ${one} and ${two} to make ${result}`);
+//     return result;
+// }
+
+// function testAll()
+// {
+//     let test = new MathLib();
+//     console.log(`Add result: ` + add(1,2));
+//     console.log('Multiply Result: ' + new MathLib().multiply(1,2));
+// }
